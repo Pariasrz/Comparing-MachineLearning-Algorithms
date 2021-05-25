41d3dcac0870cf51d97bc0e4c1614c3def8a4c91
Paria Sarzaeim <59663526+Pariasrz@users.noreply.github.com>
2021-05-25 23:25:41 +0430

    Update DTRF.py

--- DTRF.py
+++ DTRF.py
@@ -1,17 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Dec 10 15:45:02 2020
-
-@author: Pariya
-"""
-
 from sklearn.metrics import confusion_matrix
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
-from final_data import X_train, X_test, Y_train, Y_test
+from prepared_data import X_train, X_test, Y_train, Y_test
 
 
 #Decision Tree
