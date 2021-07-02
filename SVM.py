a37997d56847e9d8be5d1886706defc68022cd33
Paria Sarzaeim <59663526+Pariasrz@users.noreply.github.com>
2021-07-02 22:39:09 +0430

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -7,14 +7,24 @@ from prepared_data import X_train, X_test, Y_train, Y_test
 
 #Support Vector Machine
 from sklearn import svm
-SVM = svm.SVC(C = 100, kernel = 'rbf', gamma = 0.01, probability = False).fit(X_train, Y_train)
+SVM = svm.SVC(C = 1.0, kernel = 'rbf', gamma = 'auto', probability = False, random_state=0).fit(X_train, Y_train)
 Y_pred = SVM.predict(X_test)
+
+#Confusion Matrix
+cm = confusion_matrix(Y_test, Y_pred)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=SVM.classes_)
+disp.plot(cmap="Blues")
+
+TN = int(cm[0][0]) #true negatives
+FP = int(cm[0][1]) #false positives
 
 #Evaluation
 cm = confusion_matrix(Y_test, Y_pred)
 print("\nSupport Vector Machine")
 print('Confusion Matrix:' , cm)
 print('Accuracy: ', accuracy_score(Y_test, Y_pred)*100)
-print('Precision: ' , precision_score(Y_test, Y_pred, average='binary')*100)
-print('Recall: ', recall_score(Y_test, Y_pred, average='binary')*100)
-print('F-score: ' ,(f1_score(Y_test, Y_pred, average='binary')*100))
+print('Precision: ' , precision_score(Y_test, Y_pred)*100)
+print('Recall: ', recall_score(Y_test, Y_pred)*100)
+print('F-score: ' ,(f1_score(Y_test, Y_pred)*100))
+print('Specificity: ', (float(TN/(TN+FP)))*100)
+print('MCC: ', (matthews_corrcoef(Y_test, Y_pred))*100)
