c299d58079b12b2b19c81ea27b83d3cd45440c55
Paria Sarzaeim <59663526+Pariasrz@users.noreply.github.com>
2021-07-02 22:35:24 +0430

    Update MLP.py

--- MLP.py
+++ MLP.py
@@ -1,8 +1,4 @@
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import f1_score
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, accuracy_score, precision_score, recall_score, f1_score, matthews_corrcoef
 import pickle
 from prepared_data import X_train, X_test, Y_train, Y_test
 
@@ -15,13 +11,22 @@ Y_pred = MLP.predict(X_test)
 filename = 'finalized_model.sav'
 pickle.dump(MLP, open(filename, 'wb'))
 
-#Evaluation
+#Confusion Matrix
 cm = confusion_matrix(Y_test, Y_pred)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=DT.classes_)
+disp.plot(cmap="Blues")
+
+TN = int(cm[0][0]) #true negatives
+FP = int(cm[0][1]) #false positives
+
+#Evaluation
 print("\nMLP")
 print('Confusion Matrix:' , cm)
 print('Accuracy: ', accuracy_score(Y_test, Y_pred)*100)
 print('Precision: ' , precision_score(Y_test, Y_pred, average='binary')*100)
 print('Recall: ', recall_score(Y_test, Y_pred, average='binary')*100)
 print('F-score: ' ,(f1_score(Y_test, Y_pred, average='binary')*100))
+print('Specificity: ', (float(TN/(TN+FP)))*100)
+print('MCC: ', (matthews_corrcoef(Y_test, Y_pred))*100)
 
 loaded_model = pickle.load(open('finalized_model.sav', 'rb'))
