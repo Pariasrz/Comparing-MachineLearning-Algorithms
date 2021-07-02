000a03b4867dbe1f1ef171c5a0836bccc60d5f2a
Paria Sarzaeim <59663526+Pariasrz@users.noreply.github.com>
2021-07-02 22:42:05 +0430

    Update NB.py

--- NB.py
+++ NB.py
@@ -1,8 +1,4 @@
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import f1_score
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, accuracy_score, precision_score, recall_score, f1_score , matthews_corrcoef
 from prepared_data import X_train, X_test, Y_train, Y_test
 
 #Naive Bayes
@@ -10,11 +6,21 @@ from sklearn.naive_bayes import GaussianNB
 NB = GaussianNB()
 Y_pred = NB.fit(X_train, Y_train).predict(X_test)
 
+#Confusion Matrix
+cm = confusion_matrix(Y_test, Y_pred)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=NB.classes_)
+disp.plot(cmap="Blues")
+
+TN = int(cm[0][0]) #true negatives
+FP = int(cm[0][1]) #false positives
+
 #Evaluation
 cm = confusion_matrix(Y_test, Y_pred)
 print("\n Naive Bayes")
 print('Confusion Matrix:', cm)
 print('Accuracy: ', accuracy_score(Y_test, Y_pred)*100)
-print('Precision: ' , precision_score(Y_test, Y_pred, average = 'binary')*100)
-print('Recall: ', recall_score(Y_test, Y_pred, average = 'binary')*100)
-print('F-score: ' ,(f1_score(Y_test, Y_pred, average = 'binary')*100))
+print('Precision: ' , precision_score(Y_test, Y_pred)*100)
+print('Recall: ', recall_score(Y_test, Y_pred)*100)
+print('F-score: ' ,(f1_score(Y_test, Y_pred)*100))
+print('Specificity: ', (float(TN/(TN+FP)))*100)
+print('MCC: ', (matthews_corrcoef(Y_test, Y_pred))*100)
